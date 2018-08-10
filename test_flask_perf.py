a4cf3bd97a19d864fdd4cf1dbc6f85d38372d708
abetlen <abetlen@gmail.com>
2018-08-10 11:57:29 -0400

    Added tests to cover more cases for flask_sqlalchemy use.

--- test_flask_perf.py
+++ test_flask_perf.py
@@ -39,10 +39,28 @@ class TestFlaskProfiler(unittest.TestCase):
         app.config["PROFILER_SQLALCHEMY_ENABLED"] = True
         app.config["SQLALCHEMY_RECORD_QUERIES"] = True
         profiler = Profiler()
+        profiler.init_app(app)
 
-        with patch("flask_perf.current_app") as mock_current_app:
+        print(app.after_request_funcs)
+        self.assertTrue(profiler.__class__.log_queries in app.after_request_funcs[None])
+
+    def test_check_for_sqlalchemy(self):
+        app = Flask(__name__)
+        app.config["PROFILER_SQLALCHEMY_ENABLED"] = True
+        app.config["SQLALCHEMY_RECORD_QUERIES"] = True
+        profiler = Profiler()
+
+        with patch("flask_perf.flask_sqlalchemy_available", False), self.assertRaises(ImportError):
             profiler.init_app(app)
-            mock_current_app.after_request.assert_called_once_with(profiler.__class__.log_queries)
+
+    def test_check_for_sqlalchemy_record_queries(self):
+        app = Flask(__name__)
+        app.config["PROFILER_SQLALCHEMY_ENABLED"] = True
+        app.config["SQLALCHEMY_RECORD_QUERIES"] = False
+        profiler = Profiler()
+
+        with self.assertRaises(ValueError):
+            profiler.init_app(app)
 
 
 if __name__ == "__main__":
