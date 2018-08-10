8f79e90a2711cac9a7310b1bb55582e73d468651
abetlen <abetlen@gmail.com>
2018-08-10 12:08:58 -0400

    Bumped version to 1.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as f:
 
 setup(
     name="Flask-Perf",
-    version="1.0.1",
+    version="1.0.2",
     url="https://github.com/abetlen/Flask-Perf",
     license="MIT",
     author="Andrei Betlen",
